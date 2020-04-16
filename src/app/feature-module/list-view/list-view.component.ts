42767bd409ac9087a43500827f49132ea252363c
krsnajeeva <krsnajeeva98@gmail.com>
2020-04-16 22:06:19 +0530

    filter

--- src/app/feature-module/list-view/list-view.component.ts
+++ src/app/feature-module/list-view/list-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-list-view',
@@ -6,12 +6,14 @@ import { Component } from '@angular/core';
     styleUrls: ['./list-view.component.scss']
 })
 export class HelpDeskListViewComponent {
+    // cols: any[];
+
     sampleData = [
-        { t_no: '1', createdData: '04/15/2020', staff_name: 'sample', staff_mail: 'sample@gmail.com',program: 'Reintgration', status: 'New' ,updated_date:'04/16/2020'},
-        { t_no: '2', createdData: '04/15/2020', staff_name: 'sample', staff_mail: 'sample@gmail.com',program: 'Reintgration', status: 'In progress',updated_date:'04/16/2020' },
-        { t_no: '3', createdData: '04/15/2020', staff_name: 'sample', staff_mail: 'sample@gmail.com',program: 'Reintgration', status: 'Reopen' ,updated_date:'04/16/2020'},
-        { t_no: '4', createdData: '04/15/2020', staff_name: 'sample', staff_mail: 'sample@gmail.com',program: 'Reintgration', status: 'Duplicated' ,updated_date:'04/16/2020'},
-        { t_no: '5', createdData: '04/15/2020', staff_name: 'sample', staff_mail: 'sample@gmail.com',program: 'Reintgration',status: 'Fixed',updated_date:'04/16/2020' },
+        { t_no: '112', createdData: '04/15/2020', staff_name: 'sample', staff_mail: 'sample@gmail.com',program: 'Reintgration', status: 'New' ,updated_date:'04/16/2020'},
+        { t_no: '121', createdData: '04/15/2020', staff_name: 'sample', staff_mail: 'sample@gmail.com',program: 'Reintgration', status: 'In progress',updated_date:'04/16/2020' },
+        { t_no: 'e3323', createdData: '04/15/2020', staff_name: 'sample', staff_mail: 'sample@gmail.com',program: 'Reintgration', status: 'Reopen' ,updated_date:'04/16/2020'},
+        { t_no: '4121ss', createdData: '04/15/2020', staff_name: 'sample', staff_mail: 'sample@gmail.com',program: 'Reintgration', status: 'Duplicated' ,updated_date:'04/16/2020'},
+        { t_no: 'dsasda1', createdData: '04/15/2020', staff_name: 'sample', staff_mail: 'sample@gmail.com',program: 'Reintgration',status: 'Fixed',updated_date:'04/16/2020' },
         { t_no: '6', createdData: '04/15/2020', staff_name: 'sample', staff_mail: 'sample@gmail.com',program: 'Reintgration', status: 'New' ,updated_date:'04/16/2020'},
         { t_no: '7', createdData: '04/15/2020', staff_name: 'sample', staff_mail: 'sample@gmail.com',program: 'Reintgration', status: 'In progress' ,updated_date:'04/16/2020'},
         { t_no: '8', createdData: '04/15/2020', staff_name: 'sample', staff_mail: 'sample@gmail.com',program: 'Reintgration', status: 'Reopen' ,updated_date:'04/16/2020'},
@@ -23,4 +25,20 @@ export class HelpDeskListViewComponent {
         { t_no: '14', createdData: '04/15/2020', staff_name: 'sample', staff_mail: 'sample@gmail.com',program: 'Reintgration', status: 'Duplicated' ,updated_date:'04/16/2020'},
         { t_no: '15', createdData: '04/15/2020', staff_name: 'sample', staff_mail: 'sample@gmail.com',program: 'Reintgration', status: 'Fixed' ,updated_date:'04/16/2020'},
     ];
+
+    cols = [
+        { field: 't_no', header: 'Tickets Number' },
+        { field: 'create_date', header: 'Ticket Creation Date' },
+        { field: 's_name', header: 'Staff Name' },
+        { field: 's_email', header: 'Staff Email' },
+        { field: 'prohram', header: 'Program' },
+        { field: 'status', header: 'Status' },
+        { field: 'update_date', header: 'Updated Date' }
+    ];
+
+    onYearChange(event, sampleData) {
+       
+        sampleData.filter(event.value, 'status', 'status');
+    }
 }
+
